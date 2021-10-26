8edbdeb302da54cb7efaac7c1ad0820280ed6ecc
hataerin <goil1113@likelion.org>
2021-10-27 07:57:10 +0900

    init: SpringBoot 초기 환경 설정

---
+++ backend/welog/src/main/java/com/ssafy/welog/api/controller/dto/BoardDto.java
@@ -0,0 +1,76 @@
+package com.ssafy.welog.api.controller.dto;
+
+import com.ssafy.welog.entity.Feeling;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class BoardDto {
+
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class AddBoardReqDto {
+
+        private Long boardId;
+        private String content;
+        private String title;
+        private String version;
+        private String category;
+
+        @Builder
+        public AddBoardReqDto(Long boardId, String content, String title, String version, String category) {
+            this.boardId = boardId;
+            this.content = content;
+            this.title = title;
+            this.version = version;
+            this.category = category;
+        }
+    }
+
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PUBLIC)
+    public static class SearchBoardResDto {
+        private List<SearchBoardDto> boardList;
+
+        @Builder
+        public SearchBoardResDto(List<SearchBoardDto> boardList) {
+            this.boardList = boardList;
+        }
+    }
+    public static class SearchBoardDto{
+
+        private Long boardId;
+        private String content;
+        private LocalDateTime registerTime;
+
+        @Builder
+        public SearchBoardDto(Long boardId, String content, LocalDateTime registerTime) {
+            this.boardId = boardId;
+            this.content = content;
+            this.registerTime = registerTime;
+        }
+    }
+
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class ChangeBoardReqDto {
+        private Long boardId;
+        private String content;
+        private LocalDateTime registerTime;
+
+        @Builder
+        public ChangeBoardReqDto(Long boardId, String content, LocalDateTime registerTime) {
+            this.boardId = boardId;
+            this.content = content;
+            this.registerTime = registerTime;
+        }
+    }
+
+    
+
+}
--- backend/welog/src/main/java/com/ssafy/welog/service/BoardService.java
+++ backend/welog/src/main/java/com/ssafy/welog/service/BoardService.java
@@ -1,5 +1,42 @@
 package com.ssafy.welog.service;
 
+import com.ssafy.welog.api.controller.dto.BoardDto.AddBoardReqDto;
+import com.ssafy.welog.api.controller.dto.BoardDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+
+@Slf4j
+@Service
 public class BoardService {
+    public void addBoard(AddBoardReqDto addBoardDto){
+        log.info("게시글 등록");
+    }
+    public BoardDto.SearchBoardResDto searchAllBoard(int page){
+        log.info("게시글 전체 조회");
+        return new BoardDto.SearchBoardResDto().builder().boardList(new LinkedList<>()).build();
+    }
+
+    public BoardDto.SearchBoardResDto searchBoard(Long boardId){
+        log.info("게시글 상세 조회");
+        return new BoardDto.SearchBoardResDto().builder().boardList(new LinkedList<>()).build();
+    }
+
+
+    public void changeBoard(BoardDto.ChangeBoardReqDto changeBoardDto){
+        log.info("게시글 수정");
+    }
+    public void deleteBoard(Long boardId){
+        log.info("게시글 삭제");
+    }
+
+    public void addFavorite(Long boardId){
+        log.info("즐겨찾기 등록");
+    }
+
+    public void deleteFavorite(Long boardId){
+        log.info("즐겨찾기 삭제");
+    }
 
 }
