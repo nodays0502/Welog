8a431919f2b2f32c54e582abe40ad306469b5614
nodays0502 <nodayst@naver.com>
2021-10-27 15:19:29 +0900

    fix : admin and comment  구조 수정 및 user Auth 변경

--- backend/welog/src/main/java/com/ssafy/welog/api/controller/AdminApiController.java
+++ backend/welog/src/main/java/com/ssafy/welog/api/controller/AdminApiController.java
@@ -4,7 +4,8 @@ import static com.ssafy.welog.common.util.constants.ResponseConstants.OK;
 
 import com.ssafy.welog.api.controller.dto.AdminDto.BoardRollBackReqDto;
 import com.ssafy.welog.api.controller.dto.AdminDto.BoardRollBackResDto;
-import com.ssafy.welog.api.controller.dto.AdminDto.SearchAllUserReqDto;
+import com.ssafy.welog.api.controller.dto.AdminDto.ChangeBoardReqDto;
+import com.ssafy.welog.api.controller.dto.AdminDto.SearchAllUserResDto;
 import com.ssafy.welog.api.controller.dto.AdminDto.UserChangeReqDto;
 import com.ssafy.welog.service.AdminService;
 import lombok.extern.slf4j.Slf4j;
@@ -20,10 +21,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/admin")
 public class AdminApiController {
+
     private final AdminService adminService;
-    public AdminApiController(AdminService adminService){
+
+    public AdminApiController(AdminService adminService) {
         this.adminService = adminService;
     }
+
     /*
      * 해당 유저를 삭제하는 구현한 메서드
      */
@@ -32,25 +36,40 @@ public class AdminApiController {
         adminService.deleteUser(userId);
         return OK;
     }
+
     /*
-     * 해당 유저 권한 변경하
+     * 해당 유저 권한 변경하는 메서드
      */
     @PatchMapping("/user")
     public ResponseEntity<Void> changeUser(UserChangeReqDto userChangeReqDto) {
         adminService.changeUser(userChangeReqDto);
         return OK;
     }
-    @GetMapping("/user")
-    public ResponseEntity<SearchAllUserReqDto> searchAllUser() {
-        SearchAllUserReqDto searchAllUserReqDto = adminService.searchAllUser();
-        return ResponseEntity.ok(searchAllUserReqDto);
+
+    /*
+     * 모든 유저를 조회하는 메서드
+     */
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<SearchAllUserResDto> searchAllUser(
+        @PathVariable(required = false) Long userId) {
+        if (userId == null) {
+            return ResponseEntity.ok(adminService.searchAllUser());
+        } else {
+            return ResponseEntity.ok(adminService.searchUser(userId));
+        }
     }
 
-
-    // 글 수정 권한 변경????
+    // 게시글 권한 수정 변경하는 메서드
     @PatchMapping("/board")
-    public ResponseEntity<BoardRollBackResDto> rollBackBoard(BoardRollBackReqDto boardRollBackReqDto) {
-        BoardRollBackResDto boardRollBackResDto = adminService.rollBackBoard(boardRollBackReqDto);
-        return ResponseEntity.ok(boardRollBackResDto);
+    public ResponseEntity<Void> changeBoard(ChangeBoardReqDto changeBoardReqDto) {
+        adminService.changeBoard(changeBoardReqDto);
+        return OK;
+    }
+
+    // 게시글 롤백 하는 메서드
+    @PatchMapping("/board")
+    public ResponseEntity<BoardRollBackResDto> rollBackBoard(
+        BoardRollBackReqDto boardRollBackReqDto) {
+        return ResponseEntity.ok(adminService.rollBackBoard(boardRollBackReqDto));
     }
 }
--- backend/welog/src/main/java/com/ssafy/welog/service/CommentService.java
+++ backend/welog/src/main/java/com/ssafy/welog/service/CommentService.java
@@ -3,29 +3,51 @@ package com.ssafy.welog.service;
 import com.ssafy.welog.api.controller.dto.CommentDto.AddCommentReqDto;
 import com.ssafy.welog.api.controller.dto.CommentDto.AddFeelingtReqDto;
 import com.ssafy.welog.api.controller.dto.CommentDto.ChangeCommentReqDto;
+import com.ssafy.welog.api.controller.dto.CommentDto.SearchCommentDto;
 import com.ssafy.welog.api.controller.dto.CommentDto.SearchCommentResDto;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 public class CommentService {
-    public void addComment(AddCommentReqDto addCommentDto){
+
+    public void addComment(AddCommentReqDto addCommentDto) {
         log.info("댓글 추가");
     }
-    public SearchCommentResDto searchComment(Long BoardId){
+
+    public SearchCommentResDto searchComment(Long BoardId) {
         log.info("댓글 조회");
-        return new SearchCommentResDto().builder().commentList(new LinkedList<>()).build();
+        List<SearchCommentDto> commentList = new ArrayList<>();
+        commentList.add(SearchCommentDto.builder()
+            .commentId(1L)
+            .content("댓글1")
+            .registerTime(LocalDateTime.now())
+            .build());
+
+        commentList.add(SearchCommentDto.builder()
+            .commentId(2L)
+            .content("댓글2")
+            .registerTime(LocalDateTime.now())
+            .build());
+        return new SearchCommentResDto().builder()
+            .commentList(commentList)
+            .build();
     }
 
-    public void changeComment(ChangeCommentReqDto changeCommentDto){
+    public void changeComment(ChangeCommentReqDto changeCommentDto) {
         log.info("댓글 변경");
     }
-    public void deleteComment(Long commentId){
+
+    public void deleteComment(Long commentId) {
         log.info("댓글 삭제");
     }
-    public void addLike(AddFeelingtReqDto addFeelingtDto){
+
+    public void addLike(AddFeelingtReqDto addFeelingtDto) {
         log.info("댓글 좋아요 알림 설정까지 따르르르릉");
     }
 }
