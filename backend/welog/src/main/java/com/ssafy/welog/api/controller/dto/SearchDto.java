ebcd269c850cea5d7ac88caf9785c4622f369025
김종혁 <alb7979s@naver.com>
2021-10-29 15:30:40 +0900

    refactor: dto 내부에 static으로 선언하는 방법으로 수정

--- backend/welog/src/main/java/com/ssafy/welog/api/controller/dto/SearchDto.java
+++ backend/welog/src/main/java/com/ssafy/welog/api/controller/dto/SearchDto.java
@@ -1,17 +1,39 @@
 package com.ssafy.welog.api.controller.dto;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 public class SearchDto {
-    private Long boardId;
-    private String content;
-    private LocalDateTime registerTime;
 
-    @Builder
-    public SearchDto(Long boardId, String content, LocalDateTime registerTime) {
-        this.boardId = boardId;
-        this.content = content;
-        this.registerTime = registerTime;
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class SearchReqDto {
+        private Integer page;
+        private String searchType;
+        private String searchWord;
+
+        @Builder
+        public SearchReqDto(Integer page, String searchType, String searchWord) {
+            this.page = page;
+            this.searchType = searchType;
+            this.searchWord = searchWord;
+        }
+    }
+
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class SearchResDto {
+        private List<Object> data = new ArrayList<>();
+
+        @Builder
+        public SearchResDto(List<Object> data) {
+            this.data = data;
+        }
     }
 }
