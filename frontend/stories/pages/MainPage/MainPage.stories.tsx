59f1df6c26950b2575dbfbc5eff54bf35295e088
IMHOJEONG <39ghwjd@naver.com>
2021-11-03 18:50:59 +0900

    chore: add frontend-2

--- frontend/stories/pages/MainPage/MainPage.stories.tsx
+++ frontend/stories/pages/MainPage/MainPage.stories.tsx
@@ -4,7 +4,7 @@ import { ComponentMeta } from "@storybook/react";
 
 import styled from "styled-components";
 
-import { MainPage } from "./MainPage";
+import MainPage from "./MainPage";
 
 export default {
   component: MainPage,
